4b49f3c2ca0a994762504e3acf3b967f001b9952
Alper Dokay <aodokay@gmail.com>
2020-12-24 18:49:40 +0300

    all work merged

--- CSE3063F20P1_GRP20_iteration2/Java Project/LabelingMechanism.java
+++
@@ -1,9 +0,0 @@
-package CSE3063_Project;
-
-import java.util.ArrayList;
-
-public abstract class LabelingMechanism {
-	  
-	  public abstract Assignment label(Instance instance,User user, ArrayList<Label> labels, int instanceLabelingLimit);
-	  
-}
